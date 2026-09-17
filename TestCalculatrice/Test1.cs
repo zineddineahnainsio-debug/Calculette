cce41f10ee2fdf8d1c1c1f02d06e63c371ddaea5
Zineddine Ahnain <Zineddine Ahnain@DESKTOP-Q5G2QG5>
2026-09-17 09:54:22 +0200

    2.Tests de cas limites

--- TestCalculatrice/Test1.cs
+++ TestCalculatrice/Test1.cs
@@ -8,45 +8,45 @@ namespace TestCalculatrice
         public void TestMethod1()
         {
             double a = 5;
-            double b = 3;
-            char op = '+';
+            double b = 0;
+            char op = '/';
             double res = 8;
             Calculatrice calc = new Calculatrice();
             double test = calc.Calculer(a, b, op);
-            Assert.AreEqual(res, test);
+            Assert.AreEqual(res, test,0.001,"Division par 0 impossible");
         }
         [TestMethod]
         public void TestMethod2()
         {
             double a = 5;
-            double b = -3;
+            double b = Math.Pow(10, 1000000);
             char op = '+';
             double res = 2;
             Calculatrice calc = new Calculatrice();
             double test = calc.Calculer(a, b, op);
-            Assert.AreEqual(res, test);
+            Assert.AreEqual(res, test,0.001, "Nombre trop grand");
         }
         [TestMethod]
         public void TestMethod3()
         {
-            double a = 5;
-            double b = 0;
+            double a = Math.Pow(10, -200000);
+            double b = Math.Pow(10, -200000);
             char op = '+';
             double res = 5;
             Calculatrice calc = new Calculatrice();
             double test = calc.Calculer(a, b, op);
-            Assert.AreEqual(res, test);
+            Assert.AreEqual(res, test,0.001, "Nombre trop petit");
         }
         [TestMethod]
         public void TestMethod4()
         {
-            double a = 6;
-            double b = 0.5;
+            double a = double.PositiveInfinity;
+            double b = double.NaN;
             char op = '+';
             double res = 6.5;
             Calculatrice calc = new Calculatrice();
             double test = calc.Calculer(a, b, op);
-            Assert.AreEqual(res, test);
+            Assert.AreEqual(res, test, 0.001, "Calcul Impossible");
         }
     }
 }
